7f2b6789ac40987c8ed0ecc3df6d0b7bdc28bea0
mohamadhasan <mhtaghad99@gmail.com>
2019-06-06 15:37:10 +0430

    add createdb func

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+)
 
 //scan URL
 func getURL() string {
 	var inurl string
 	fmt.Scanf("%s", &inurl)
 	return inurl
+}
+func createdb() {
+	os.Create("./data.db")
+	database, _ := sql.Open("sqlite3", "./data.db")
+	statement, _ := database.Prepare("create table if not exists URLs(id integer primary key, url varchar, shorturl varchar ,exptime date ,redirnum int )")
+	statement.Exec()
+
 }
 func main() {
 
