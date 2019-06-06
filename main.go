687b72927374b6a9edb2eda57125f81d99467bd7
mohamadhasan <mhtaghad99@gmail.com>
2019-06-07 02:00:16 +0430

    just for fun funcs were damaged

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
-	"os"
 	"strconv"
 )
 
@@ -23,11 +22,20 @@ func createshorturl(id int) string {
 
 //data base func :
 //first func : create data base & table
+//now this is not good func
+//daghoonesh kardam ke ye insert dorost dashte basham
 func createdb() *sql.DB {
-	os.Create("./data.db")
 	database, _ := sql.Open("sqlite3", "./data.db")
-	statement, _ := database.Prepare("create table if not exists URLs(id integer primary key autoincrement, url varchar , shorturl varchar , redircount int )")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS urls(id INTEGER PRIMARY KEY , url TEXT, shorturl TEXT, redircount INTEGER )")
 	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO URLs (url, shorturl, redirvount) VALUES (?, ?, ?)")
+	statement.Exec("gogo.com", "go.com", 0)
+	st, _ := database.Query("select shorturl from urls")
+	var shr string
+	for st.Next() {
+		st.Scan(&shr)
+		fmt.Println(shr)
+	}
 	return database
 
 }
@@ -35,34 +43,33 @@ func createdb() *sql.DB {
 //second func : insert to data base if is not exist
 func insertdb(id int, inurl string, db *sql.DB) string {
 	var res string
-	//var id1,i int
-	//var urll string
-	statement, err := db.Query("select shorturl from URLs where url = ?", inurl)
+	var id2 int
 
-	for statement.Next() {
-		statement.Scan(&res)
-	}
+	statement, err := db.Query("SELECT id from URLs")
 
+	statement.Scan(&id2)
+	fmt.Println(id2)
+	//}
 	if err == sql.ErrNoRows {
-		println("has no same url")
+		fmt.Println("has no same url")
 		res = createshorturl(id)
-		st, error := db.Prepare("insert into URLs(url,shorturl,redirvount) values (?, ?,0)")
-		st.Exec(inurl, res)
+		st, error := db.Prepare("insert into URLs (url, shorturl, redirvount) values (?, ?, ?)")
+		st.Exec(inurl, res, 0)
 		if error != nil {
-			println(error)
+			println("injaaaaaaaaaaa")
 		}
-		return res
+		//	return res
 	}
 
-	//fmt.Println("eeee alie")
-	println(res)
-	return res
+	//println(res)
+	return strconv.Itoa(id2)
 }
 
 func main() {
-	inurl := getURL()
-	db := createdb()
-	shorturl := insertdb(10, inurl, db)
-	fmt.Println(shorturl)
+	//inurl := getURL()
+	//db := createdb()
+	//shorturl := insertdb(1000, inurl, db)
+	//fmt.Println(shorturl)
+	createdb()
 
 }
