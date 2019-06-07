4670d3b2104f303b313d616eaa46cb8944227b15
mohamadhasan <mhtaghad99@gmail.com>
2019-06-07 19:27:59 +0430

    work on api

--- main.go
+++ main.go
@@ -3,18 +3,35 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"math/rand"
+	"strconv"
+
 	_ "github.com/mattn/go-sqlite3"
 	"log"
-	"math/rand"
 	"net/http"
-	"strconv"
 )
 
+var db sql.DB
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	keys, ok := r.URL.Query()["key"]
+
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'key' is missing")
+		return
+	}
+
+	// Query()["key"] will return an array of items,
+	// we only want the single item.
+	key := keys[0]
+
+	log.Println("Url Param 'key' is: " + string(key))
+}
 func redirect(w http.ResponseWriter, r *http.Request) {
+
 	http.Redirect(w, r, "http://www.google.com", 301)
 }
-
-var db sql.DB
 
 //scan URL
 func getURL() string {
@@ -25,18 +42,18 @@ func getURL() string {
 
 //this is for check genrated short url is exist or not
 func ifexistShorturl(shorturl string) bool {
-	stQuery := "SELECT shorturl from urls where shorturl =" + "'" + shorturl + "'"
+	var result string
+	stQuery := "SELECT shorturl FROM urls WHERE shorturl =" + "'" + shorturl + "'"
 	res, err := db.Query(stQuery)
 	if err != nil {
 		fmt.Println(err)
 	}
 	for res.Next() {
-		res.Scan(&shorturl)
-		fmt.Println(shorturl)
+		res.Scan(&result)
+		fmt.Println(result)
 	}
 	//we have bug here
-	///we have problem here to know result of query is emty or not
-	if shorturl == "" {
+	if result == "" {
 
 		//fmt.Println("voojood nadarad")
 		return false
@@ -47,7 +64,7 @@ func ifexistShorturl(shorturl string) bool {
 
 //create shortURL
 func createshorturl(id int) string {
-	if !ifexistShorturl("taghad.gogo/" + strconv.Itoa(297)) {
+	if !ifexistShorturl("taghad.gogo/" + strconv.Itoa(id)) {
 		return "taghad.gogo/" + strconv.Itoa(id)
 	}
 	return createshorturl(rand.Int())
@@ -61,7 +78,7 @@ func createdb() *sql.DB {
 	if errorOpen != nil {
 		fmt.Println(errorOpen)
 	}
-	statement, err := db.Prepare("CREATE TABLE IF NOT EXISTS urls(id INTEGER PRIMARY KEY , url text , shorturl text, redircount INTEGER )")
+	statement, err := db.Prepare("CREATE TABLE IF NOT EXISTS urls(id INTEGER PRIMARY KEY , url text , shorturl text,exptime date , redircount INTEGER )")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -80,7 +97,6 @@ func insertdb(shorturl string, inurl string) string {
 	//if exist don't insert it
 	stQuery := "SELECT shorturl from urls where url =" + "'" + inurl + "'"
 	statement, err := db.Query(stQuery)
-	///we have problem here to know result of query is emty or not
 	if err != nil {
 		fmt.Println(err)
 	} else {
@@ -96,7 +112,7 @@ func insertdb(shorturl string, inurl string) string {
 
 	fmt.Println(err)
 	res = shorturl
-	st, error := db.Prepare("insert into urls (url, shorturl, redircount) values (?,?,?)")
+	st, error := db.Prepare("insert into urls (url, shorturl,exptime , redircount) values (?,?, DATEADD(min ,30, current_date),?)")
 
 	if error != nil {
 		fmt.Println(error)
@@ -127,10 +143,8 @@ func insWithCusShorturl(custshort string, inurl string) bool {
 }
 
 func main() {
-
-	//i comented this part foe learning restful api
 	{
-		/*fmt.Println("1: give shorturl \n2: redirect with shorturl \n3: set short url for your link")
+		fmt.Println("1: give shorturl \n2: redirect with shorturl \n3: set short url for your link")
 		var state int
 		fmt.Scanf("%d", &state)
 		switch state {
@@ -141,10 +155,7 @@ func main() {
 			fmt.Println(shorturl)
 			break
 		case 2:
-			var shorturl string
-			fmt.Scan(&shorturl)
-			fmt.Println(shorturl)
-			http.HandleFunc("/gogo", redirect)
+			http.HandleFunc("/", redirect)
 			err := http.ListenAndServe(":9090", nil)
 			if err != nil {
 				log.Fatal("ListenAndServe: ", err)
@@ -154,11 +165,6 @@ func main() {
 			inurl := getURL()
 			db = *createdb()
 			insWithCusShorturl(getURL(), inurl)
-		}*/
-		http.HandleFunc("/", redirect)
-		err := http.ListenAndServe(":9090", nil)
-		if err != nil {
-			log.Fatal("ListenAndServe: ", err)
 		}
 
 	}
